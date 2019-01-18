dea46b5501dfc3ecd94103ef6c50a4762929f36e
Eric Willis <eric@ecwillis.com>
2019-01-18 22:33:39 +0000

    Initial V

---
+++ src/Client.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace InstagramLatest;
+
+use GuzzleHttp\Client as GClient;
+
+class Client {
+  protected $username;
+  protected $gc;
+  public function  __construct($un) {
+    $this->username = $un;
+    $this->gc = new GClient([
+      'base_uri' => 'https://www.instagram.com',
+      'timeout' => 30.0
+    ]);
+
+    $this->gce = new GClient([
+      'base_uri' => 'https://api.instagram.com',
+      'timeout' => 30.0
+    ]);
+  }
+
+  public function host() {
+    return "/{$this->username}";
+  }
+
+  protected function _extractIds($payload) {
+    if (strlen($payload) < 100) {
+      throw new \Error('Invalid Payload');
+    }
+    preg_match_all('/"shortcode":"([A-Za-z]\w+)"/', $payload, $out);
+
+    return $out;
+  }
+
+  protected function _handleResponse($r) {
+    if ($r->getStatusCode() !== 200) {
+      // Log this...
+      //throw new \Error('Resource Error');
+      return [];
+    }
+    $body = $r->getBody();
+    $payload = $body->getContents();
+    return json_decode($payload);
+  }
+
+  protected function _getEmbeds($shortcodes) {
+    $return = [];
+    foreach($shortcodes as $code) {
+      $resp = $this->gce->get('oembed', [
+        'query' => [
+          'omitscript' => 'true',
+          'url' => "http://instagr.am/p/{$code}"
+        ]
+      ]);
+
+      $js = $this->_handleResponse($resp);
+      $return[] = $js;
+    }
+
+    return $return;
+  }
+
+  public function getPosts() {
+    $resp = $this->gc->get($this->username, [
+      'headers' => [
+        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+      ]
+    ]);
+    if ($resp->getStatusCode() !== 200) {
+      throw new \Error('User not found');
+    }
+
+    $body = $resp->getBody();
+    $payload = $body->getContents();
+
+    $shortCodes = $this->_extractIds($payload);
+  
+    return $this->_getEmbeds($shortCodes[1]);
+  }
+}
